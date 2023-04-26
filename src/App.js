b48c86f83516c9d476f28bd223c0964acbfd8772
David Avery <avery.di@gmail.com>
2023-04-26 15:46:12 +0000

    Folder Edits

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar';
+
+function App() {
+    return (
+        <div className='App'>
+            <Navbar />
+            <div className='Jumbortron'>
+                <h1>Jumbotron</h1>
+            </div>
+        </div>
+
+    );
+}
+export default App;
---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Navbar Here</h1>
+            <div className="'links">
+                <a href="/">Home</a>
+                <a href=".Contact">Contact</a>
+            </div>
+        </nav>
+    );
+}
+ 
+export default Navbar;
